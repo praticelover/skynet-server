2a68070573495639b80a99b1bf216d77066abeba
Joker <praticelover@gmail.com>
2022-10-18 21:43:32 +0800

    lualib update

--- lualib/random.lua
+++ lualib/random.lua
@@ -7,7 +7,7 @@
 
 
 local skynet = require "skynet"
-local mt19937 = require "mt19937"
+--local mt19937 = require "mt19937"
 local random = {}
 
 local random_type = tonumber(skynet.getenv("random_type"))
@@ -16,15 +16,15 @@ local __randomseed = os.time() + skynet.self()
 
 function random.rand(i, j)
 	__randomseed = __randomseed + 1
-	if random_type == RANDOM_TYPE.mt_19937 then --随机性低,快
-		mt19937.init(__randomseed)
-		if not j then
-			j = i + 1 
-			return mt19937.randi(1, i+1)
-		else
-			return mt19937.randi(i, j+1)
-		end
-	elseif random_type == RANDOM_TYPE.linux_urandom then --随机性中,快
+	--if random_type == RANDOM_TYPE.mt_19937 then --随机性低,快
+	--	mt19937.init(__randomseed)
+	--	if not j then
+	--		j = i + 1
+	--		return mt19937.randi(1, i+1)
+	--	else
+	--		return mt19937.randi(i, j+1)
+	--	end
+	if random_type == RANDOM_TYPE.linux_urandom then --随机性中,快
 		local data = io.open("/dev/urandom", "r"):read(4)
         math.randomseed(os.time() + data:byte(1) + (data:byte(2) * 256) + (data:byte(3) * 65536) + (data:byte(4) * 4294967296))
         return math.random(i, j)
